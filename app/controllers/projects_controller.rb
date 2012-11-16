29deffe8c819bdc18eeb00bb50990da2abcf37c9
Tanner Welsh <tanner@openspectrumstudios.com>
2012-11-16 00:52:54 -0800

    Slight change to projects/index

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,7 +2,7 @@ class ProjectsController < ApplicationController
   # GET /projects
   # GET /projects.json
   def index
-    @projects = Project.all
+    @projects = Project.includes(:members)
 
     respond_to do |format|
       format.html # index.html.erb
