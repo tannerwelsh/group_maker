69b84b4790fee08c1fac605b8179a3d649cf87cf
Tanner Welsh <tanner@openspectrumstudios.com>
2012-11-20 10:00:35 -0800

    Install devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 GroupMaker::Application.routes.draw do
 
+  devise_for :users
+
   root to: 'projects#index'
   
   resources :users
--- db/migrate/20121116060530_create_users.rb
+++ db/migrate/20121116060530_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :name, null: false
-      t.string :email
+      t.string  :name,        null: false
+      t.string  :email,       null: false, default: ""
       t.integer :project_id
 
       t.timestamps
