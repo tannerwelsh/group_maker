b0ff0a1aec230669087826422a84d37c426a2a89
Tanner Welsh <tanner@openspectrumstudios.com>
2012-11-20 20:45:44 -0800

    Views for voting

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,9 @@
 module ProjectsHelper
+  def upvote_link(project)
+    unless current_user.voted_on? project
+      link_to raw('<i class="icon-arrow-up"></i>'), upvote_project_path(project), method: :post, remote: true
+    else
+      raw('<i class="icon-minus"></i>')
+    end
+  end
 end
