beb86131512d515986b445c2bc2cc11ea4b6b492
Tanner Welsh <tanner@openspectrumstudios.com>
2012-11-21 11:39:21 -0800

    Notify users with the group they've been selected for

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,7 @@ class PagesController < ApplicationController
   def home
     @projects = Project.sorted_by_votes
     @users    = User.alphabetized.includes(:choices)
+
+    @user_project = current_user.project if user_signed_in? && current_user.has_project?
   end
 end
