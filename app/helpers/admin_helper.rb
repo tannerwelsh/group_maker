a99824ff041d11ec6e11cfcad84ba3bb344a125b
Tanner Welsh <tanner@openspectrumstudios.com>
2012-11-21 19:44:07 -0800

    Small refactor in AdminHelper

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,9 +1,13 @@
 module AdminHelper
   def toggle_link_for(config)
     toggle_type = Settings.send(config) ? 'Disable' : 'Enable'
-    link_text   = toggle_type + ' ' + config.capitalize
+    link_text   = toggle_type + ' ' + snake_to_string(config)
 
     link_to link_text, toggle_path(config), method: :post, class: "btn #{setting_class(config)}"
+  end
+
+  def snake_to_string(string)
+    string.to_s.split('_').map(&:capitalize).join(' ')
   end
 
   def setting_class(config)
